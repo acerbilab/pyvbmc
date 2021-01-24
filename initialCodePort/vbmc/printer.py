42811ae0e5bfed7c337c76d284456459437fedc5
Marlon Tobaben <marlon.tobaben@helsinki.fi>
2021-01-24 17:19:17 +0200

    feat: add vbmc printer class

---
+++ initialCodePort/vbmc/printer.py
@@ -0,0 +1,17 @@
+class Printer(object):
+    """
+    Prints the status messages of the VBMC algorithm
+    """
+
+    def __init__(self, verbose_level):
+        self.verbose_level = verbose_level
+
+    def print_final_message(self, exitflag):
+        #Print final message
+        if verbose_level > 1:
+            #fprintf('\n%s\n', msg);    
+            #fprintf('Estimated ELBO: %.3f +/- %.3f.\n', elbo, elbo_sd);
+            #if exitflag < 1
+            #    fprintf('Caution: Returned variational solution may have not converged.\n');
+            #fprintf('\n');
+            print('final message')
