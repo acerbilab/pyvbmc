897f2109602ad176fa435e0cdd599eec6bc7fa8a
Marlon Tobaben <24623119+Solosneros@users.noreply.github.com>
2021-02-25 14:59:51 +0200

    fix: rename bound to bounds in ParameterTransformer

--- initialCodePort/parameter_transformer/parameter_transformer.py
+++ initialCodePort/parameter_transformer/parameter_transformer.py
@@ -7,10 +7,10 @@ class ParameterTransformer:
     def __init__(
         self,
         nvars: int,
-        lower_bound: np.ndarray = None,
-        upper_bound: np.ndarray = None,
-        plausible_lower_bound: np.ndarray = None,
-        plausible_upper_bound: np.ndarray = None,
+        lower_bounds: np.ndarray = None,
+        upper_bounds: np.ndarray = None,
+        plausible_lower_bounds: np.ndarray = None,
+        plausible_upper_bounds: np.ndarray = None,
     ):
         """
         __init__ [summary]
@@ -19,52 +19,52 @@ class ParameterTransformer:
         ----------
         nvars : int
             [description]
-        lower_bound : np.ndarray, optional
+        lower_bounds : np.ndarray, optional
             [description], by default None
-        upper_bound : np.ndarray, optional
+        upper_bounds : np.ndarray, optional
             [description], by default None
-        plausible_lower_bound : np.ndarray, optional
+        plausible_lower_bounds : np.ndarray, optional
             [description], by default None
-        plausible_upper_bound : np.ndarray, optional
+        plausible_upper_bounds : np.ndarray, optional
             [description], by default None
         """
         # Empty LB and UB are Infs
-        if lower_bound is None:
-            lower_bound = np.ones(nvars) * -np.inf
-        if upper_bound is None:
-            upper_bound = np.ones(nvars) * np.inf
+        if lower_bounds is None:
+            lower_bounds = np.ones(nvars) * -np.inf
+        if upper_bounds is None:
+            upper_bounds = np.ones(nvars) * np.inf
 
         # Empty plausible bounds equal hard bounds
-        if plausible_lower_bound is None:
-            plausible_lower_bound = np.copy(lower_bound)
-        if plausible_upper_bound is None:
-            plausible_upper_bound = np.copy(upper_bound)
+        if plausible_lower_bounds is None:
+            plausible_lower_bounds = np.copy(lower_bounds)
+        if plausible_upper_bounds is None:
+            plausible_upper_bounds = np.copy(upper_bounds)
 
         # Convert scalar inputs to row vectors (I do not think it is necessary)
 
         if (
-            not np.all(lower_bound <= plausible_lower_bound)
-            and np.all(plausible_lower_bound < plausible_upper_bound)
-            and np.all(plausible_upper_bound <= upper_bound)
+            not np.all(lower_bounds <= plausible_lower_bounds)
+            and np.all(plausible_lower_bounds < plausible_upper_bounds)
+            and np.all(plausible_upper_bounds <= upper_bounds)
         ):
             sys.error(
                 "Variable bounds should be LB <= PLB < PUB <= UB for all variables."
             )
 
         # Transform to log coordinates
-        self.lower_bound_orig = lower_bound
-        self.upper_bound_orig = upper_bound
+        self.lower_bounds_orig = lower_bounds
+        self.upper_bounds_orig = upper_bounds
 
         self.type = np.zeros((nvars))
         for i in range(nvars):
-            if np.isfinite(lower_bound[i]) and np.isinf(upper_bound[i]):
+            if np.isfinite(lower_bounds[i]) and np.isinf(upper_bounds[i]):
                 self.type[i] = 1
-            if np.isinf(lower_bound[i]) and np.isfinite(upper_bound[i]):
+            if np.isinf(lower_bounds[i]) and np.isfinite(upper_bounds[i]):
                 self.type[i] = 2
             if (
-                np.isfinite(lower_bound[i])
-                and np.isfinite(upper_bound[i])
-                and lower_bound[i] < upper_bound[i]
+                np.isfinite(lower_bounds[i])
+                and np.isfinite(upper_bounds[i])
+                and lower_bounds[i] < upper_bounds[i]
             ):
                 self.type[i] = 3
 
@@ -73,19 +73,19 @@ class ParameterTransformer:
         self.delta = np.ones(nvars)
 
         # Get transformed PLB and ULB
-        plausible_lower_bound = self.direct_transform(plausible_lower_bound)
-        plausible_upper_bound = self.direct_transform(plausible_upper_bound)
+        plausible_lower_bounds = self.direct_transform(plausible_lower_bounds)
+        plausible_upper_bounds = self.direct_transform(plausible_upper_bounds)
 
         # Center in transformed space
         for i in range(nvars):
-             if np.isfinite(plausible_lower_bound[i]) and np.isfinite(
-                 plausible_upper_bound[i]
+             if np.isfinite(plausible_lower_bounds[i]) and np.isfinite(
+                 plausible_upper_bounds[i]
              ):
                  self.mu[i] = 0.5 * (
-                     plausible_lower_bound[i] + plausible_upper_bound[i]
+                     plausible_lower_bounds[i] + plausible_upper_bounds[i]
                  )
                  self.delta[i] = (
-                     plausible_lower_bound[i] - plausible_upper_bound[i]
+                     plausible_lower_bounds[i] - plausible_upper_bounds[i]
                  )
 
     def direct_transform(self, constrained_variables: np.ndarray):
@@ -105,20 +105,20 @@ class ParameterTransformer:
 
         unconstrained_variables = np.copy(constrained_variables)
 
-        # Unbounded scalars (possibly center and rescale)
+        # Unboundsed scalars (possibly center and rescale)
         mask = self.type == 0
         if np.any(mask):
             unconstrained_variables[mask] = (
                 constrained_variables[mask] - self.mu[mask]
             ) / self.delta[mask]
 
-        # Lower and upper bounded scalars
+        # Lower and upper boundsed scalars
         mask = self.type == 3
         if np.any(mask):
             z = (
                 constrained_variables[mask]
-                - self.lower_bound_orig / self.upper_bound_orig
-                - self.lower_bound_orig
+                - self.lower_bounds_orig / self.upper_bounds_orig
+                - self.lower_bounds_orig
             )
             unconstrained_variables[mask] = np.log(z / (1 - z))
             unconstrained_variables[mask] = (
--- initialCodePort/parameter_transformer/test_parameter_transformer.py
+++ initialCodePort/parameter_transformer/test_parameter_transformer.py
@@ -12,55 +12,55 @@ __Init__ method
 """
 NVARS = 3
 
-def test_init_no_lower_bound():
+def test_init_no_lower_bounds():
     parameter_transformer = ParameterTransformer(nvars=NVARS)
-    assert np.all(np.isinf(parameter_transformer.lower_bound_orig))
+    assert np.all(np.isinf(parameter_transformer.lower_bounds_orig))
 
 
-def test_init_lower_bound():
+def test_init_lower_bounds():
     parameter_transformer = ParameterTransformer(
-        nvars=NVARS, lower_bound=np.ones(NVARS)
+        nvars=NVARS, lower_bounds=np.ones(NVARS)
     )
-    assert np.all(parameter_transformer.lower_bound_orig == np.ones(NVARS))
+    assert np.all(parameter_transformer.lower_bounds_orig == np.ones(NVARS))
 
 
-def test_init_no_upper_bound():
+def test_init_no_upper_bounds():
     parameter_transformer = ParameterTransformer(nvars=NVARS)
-    assert np.all(np.isinf(parameter_transformer.upper_bound_orig))
+    assert np.all(np.isinf(parameter_transformer.upper_bounds_orig))
 
 
-def test_init_upper_bound():
+def test_init_upper_bounds():
     parameter_transformer = ParameterTransformer(
-        nvars=NVARS, upper_bound=np.ones(NVARS)
+        nvars=NVARS, upper_bounds=np.ones(NVARS)
     )
-    assert np.all(parameter_transformer.upper_bound_orig == np.ones(NVARS))
+    assert np.all(parameter_transformer.upper_bounds_orig == np.ones(NVARS))
 
 def test_init_type_1():
     parameter_transformer = ParameterTransformer(
         nvars=NVARS,
-        lower_bound=np.ones(NVARS) * -np.inf,
-        upper_bound=np.ones(NVARS) * np.inf,
+        lower_bounds=np.ones(NVARS) * -np.inf,
+        upper_bounds=np.ones(NVARS) * np.inf,
     )
     assert np.all(parameter_transformer.type == np.zeros(NVARS))
 
 
 def test_init_type_1():
     parameter_transformer = ParameterTransformer(
-        nvars=NVARS, lower_bound=np.ones(NVARS), upper_bound=np.ones(NVARS) * np.inf
+        nvars=NVARS, lower_bounds=np.ones(NVARS), upper_bounds=np.ones(NVARS) * np.inf
     )
     assert np.all(parameter_transformer.type == np.ones(NVARS) * 1)
 
 
 def test_init_type_2():
     parameter_transformer = ParameterTransformer(
-        nvars=NVARS, lower_bound=np.ones(NVARS) * -np.inf, upper_bound=np.ones(NVARS)
+        nvars=NVARS, lower_bounds=np.ones(NVARS) * -np.inf, upper_bounds=np.ones(NVARS)
     )
     assert np.all(parameter_transformer.type == np.ones(NVARS) * 2)
 
 
 def test_init_type_3():
     parameter_transformer = ParameterTransformer(
-        nvars=NVARS, lower_bound=np.ones(NVARS), upper_bound=np.ones(NVARS)*2
+        nvars=NVARS, lower_bounds=np.ones(NVARS), upper_bounds=np.ones(NVARS)*2
     )
     assert np.all(parameter_transformer.type == np.ones(NVARS) * 3)
 
