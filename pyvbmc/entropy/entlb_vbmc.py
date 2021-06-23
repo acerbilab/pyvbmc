fix: correct w_grad computation and test files

pyvbmc/entropy/entlb_vbmc.py
@@ -16,8 +16,7 @@ def entlb_vbmc(
     grad_flags : tuple of bool, len(grad_flags)=4, optional
         Whether to compute the gradients for [mu, sigma, lambda, w].
     jacobian_flag : bool, optional
-        Whether variational parameters are transformed.
-        The variational parameters and corresponding transformations are:
+        Whether variational parameters are transformed. The variational parameters and corresponding transformations are:
         sigma (log), lambda (log), w (softmax).
 
     Returns
@@ -34,11 +33,7 @@ def entlb_vbmc(
 
     References
     ----------
-    .. [1] Gershman, S. J., Hoffman, M. D., & Blei, D. M. (2012).
-        Nonparametric variational inference. Proceedings of the 29th
-        International Coference on International Conference on Machine Learning,
-        235–242.
-
+    .. [1]: Gershman, S. J., Hoffman, M. D., & Blei, D. M. (2012). Nonparametric variational inference. Proceedings of the 29th International Coference on International Conference on Machine Learning, 235–242.
     """
     BigK = np.inf  # large number of components
 
@@ -65,11 +60,18 @@ def entlb_vbmc(
             + np.log(lambd).sum()
         )
 
+        if grad_flags[0]:
+            mu_grad = np.zeros(D)
+
         if grad_flags[1]:
             sigma_grad = D / sigma
 
         if grad_flags[2]:
             lambd_grad = 1 / lambd
+
+        if grad_flags[3]:
+            w_grad = np.zeros(1)
+
     elif K > BigK:
         raise NotImplementedError("Not implemented yet for K > BigK.")
     else:

pyvbmc/entropy/entmc_vbmc.py
@@ -1,6 +1,5 @@
 import numpy as np
 from pyvbmc.variational_posterior import VariationalPosterior
-from pyvbmc.utils.testing import randn2
 
 
 def entmc_vbmc(
@@ -20,7 +19,7 @@ def entmc_vbmc(
     grad_flags : tuple of bool, len(grad_flags)=4, default=tuple([True] * 4)
         Whether to compute the gradients for [mu, sigma, lambda, w].
     jacobian_flag : bool
-        Whether variational parameters are transformed. 
+        Whether variational parameters are transformed.
         The variational parameters and corresponding transformations are:
         sigma (log), lambda (log), w (softmax).
 
@@ -29,7 +28,7 @@ def entmc_vbmc(
     H: float
         Estimated entropy of vp by Monte Carlo method.
     dH: np.ndarray
-        Estimated entropy gradient by Monte Carlo method. 
+        Estimated entropy gradient by Monte Carlo method.
         :math:`dH = \left[\nabla_{\mu_1}^{T} H, ..., \nabla_{\mu_K}^{T} H,
         \nabla_{\sigma}^{T} H, \nabla_{\lambda}^{T} H,
         \nabla_{\omega}^{T} H\right]`
@@ -64,9 +63,6 @@ def entmc_vbmc(
         # Draw Monte Carlo samples from the j-th component
         # Antithetic sampling
         epsilon[: Ns // 2, :] = np.random.randn(Ns // 2, D)
-        # epsilon[: Ns // 2, :] = randn2(
-        #     D, Ns // 2
-        # ).transpose()  # For exact testing with MATLAB
         epsilon[Ns // 2 :, :] = -epsilon[: Ns // 2, :]
 
         Xs = epsilon * lambd * sigma[j] + mu[:, j]  # [Ns, D]
@@ -111,7 +107,7 @@ def entmc_vbmc(
 
             if grad_flags[3]:
                 w_grad[j] -= np.log(q_j).sum() / Ns
-                w_grad[:] -= w[j] * (norm_j1[:, j] / q_j).sum() / Ns
+                w_grad[:] -= (w[j] * norm_j1 / q_j[:, None]).sum(0) / Ns
 
     # Correct for standard log reparameterization of SIGMA
     if jacobian_flag and grad_flags[1]:

pyvbmc/entropy/test_entlb_vbmc.py
@@ -8,7 +8,7 @@ from pyvbmc.utils.testing import check_grad
 def entlb_vbmc_wrapper(theta, D, K, ret="H"):
     assert theta.shape[0] == D * K + K + D + K
     vp = VariationalPosterior(D, K)
-    vp.mu = np.reshape(theta[: D * K], (D, K))
+    vp.mu = np.reshape(theta[: D * K], (D, K), "F")
     vp.sigma = theta[D * K : D * K + K]
     vp.lambd = theta[D * K + K : D * K + K + D]
     vp.w = theta[D * K + K + D :]
@@ -19,13 +19,27 @@ def entlb_vbmc_wrapper(theta, D, K, ret="H"):
         )
         return H
     else:
-        _, dH = entlb_vbmc(
-            vp, grad_flags=tuple([True] * 4), jacobian_flag=False
-        )
+        _, dH = entlb_vbmc(vp, grad_flags=(True,) * 4, jacobian_flag=False)
         return dH
 
 
-def test_entlb_vbmc_multi():
+def test_entlb_vbmc_single_gaussian():
+    # Check gradients with a single Gaussian
+    D, K = 3, 1
+    vp = VariationalPosterior(D, K)
+    vp.mu = np.ones((D, K))
+    vp.sigma = np.ones((1, K))
+
+    Hl, dHl = entlb_vbmc(vp, jacobian_flag=False)
+    theta0 = np.concatenate(
+        [x.flatten() for x in [vp.mu.transpose(), vp.sigma, vp.lambd, vp.w]]
+    )
+    f = lambda theta: entlb_vbmc_wrapper(theta, D, K, "H")
+    f_grad = lambda theta: entlb_vbmc_wrapper(theta, D, K, "dH")
+    assert check_grad(f, f_grad, theta0, rtol=0.01)
+
+
+def test_entlb_vbmc_nonoverlapping_mixture():
     # Check with multiple Gaussians that nearly have non-overlapping supports
     D, K = 3, 2
     vp = VariationalPosterior(D, K)
@@ -53,7 +67,26 @@ def test_entlb_vbmc_multi():
 
     # Check gradients
     theta0 = np.concatenate(
-        [x.flatten() for x in [vp.mu, vp.sigma, vp.lambd, vp.w]]
+        [x.flatten() for x in [vp.mu.transpose(), vp.sigma, vp.lambd, vp.w]]
+    )
+    f = lambda theta: entlb_vbmc_wrapper(theta, D, K, "H")
+    f_grad = lambda theta: entlb_vbmc_wrapper(theta, D, K, "dH")
+    assert check_grad(f, f_grad, theta0, rtol=0.01)
+
+
+def test_entlb_vbmc_overlapping_mixture():
+    # Check gradients with multiple Gaussians that have overlapping supports
+    np.random.seed(42)
+    D, K = 3, 2
+    vp = VariationalPosterior(D, K)
+    vp.mu = np.random.uniform(-1, 1, size=(D, K))
+    vp.sigma = np.ones(K) + 0.2 * np.random.rand(K)
+    vp.lambd = np.ones(D) + 0.2 * np.random.rand(D)
+    vp.eta = np.random.rand(K)
+    vp.w = np.exp(vp.eta) / np.exp(vp.eta).sum()
+
+    theta0 = np.concatenate(
+        [x.flatten() for x in [vp.mu.transpose(), vp.sigma, vp.lambd, vp.w]]
     )
     f = lambda theta: entlb_vbmc_wrapper(theta, D, K, "H")
     f_grad = lambda theta: entlb_vbmc_wrapper(theta, D, K, "dH")

pyvbmc/entropy/test_entmc_vbmc.py
@@ -25,7 +25,7 @@ def single_gaussian_entropy(D, sigma, lambd):
 def entmc_vbmc_wrapper(theta, D, K, Ns=1e5, ret="H"):
     assert theta.shape[0] == D * K + K + D + K
     vp = VariationalPosterior(D, K)
-    vp.mu = np.reshape(theta[: D * K], (D, K))
+    vp.mu = np.reshape(theta[: D * K], (D, K), "F")
     vp.sigma = theta[D * K : D * K + K]
     vp.lambd = theta[D * K + K : D * K + K + D]
     vp.w = theta[D * K + K + D :]
@@ -43,7 +43,7 @@ def entmc_vbmc_wrapper(theta, D, K, Ns=1e5, ret="H"):
         return dH
 
 
-def test_entmc_vbmc_single():
+def test_entmc_vbmc_single_gaussian():
     # Check with a single Gaussian
     D, K, Ns = 3, 1, 1e5
     vp = VariationalPosterior(D, K)
@@ -58,14 +58,14 @@ def test_entmc_vbmc_single():
 
     # Check gradients
     theta0 = np.concatenate(
-        [x.flatten() for x in [vp.mu, vp.sigma, vp.lambd, vp.w]]
+        [x.flatten() for x in [vp.mu.transpose(), vp.sigma, vp.lambd, vp.w]]
     )
     f = lambda theta: entmc_vbmc_wrapper(theta, D, K, Ns, "H")
     f_grad = lambda theta: entmc_vbmc_wrapper(theta, D, K, Ns, "dH")
     assert check_grad(f, f_grad, theta0, rtol=0.01)
 
 
-def test_entmc_vbmc_multi():
+def test_entmc_vbmc_nonoverlapping_mixture():
     # Check with multiple Gaussians that nearly have non-overlapping supports
     Ns = 1e5
     for D in range(1, 3):
@@ -89,7 +89,7 @@ def test_entmc_vbmc_multi():
                     1 / vp.lambd.flatten() * vp.w[k]
                 )  # lambda
                 dH_appro[D * K + K + D + k] = (
-                    H_appro_k - K - np.log(vp.w[k])
+                    H_appro_k - 1 - np.log(vp.w[k])
                 )  # w
 
             H, dH = entmc_vbmc(vp, Ns, jacobian_flag=False)
@@ -99,17 +99,41 @@ def test_entmc_vbmc_multi():
 
             # Check gradients
             theta0 = np.concatenate(
-                [x.flatten() for x in [vp.mu, vp.sigma, vp.lambd, vp.w]]
+                [
+                    x.flatten()
+                    for x in [vp.mu.transpose(), vp.sigma, vp.lambd, vp.w]
+                ]
             )
             f = lambda theta: entmc_vbmc_wrapper(theta, D, K, Ns, "H")
             f_grad = lambda theta: entmc_vbmc_wrapper(theta, D, K, Ns, "dH")
             check_grad(f, f_grad, theta0, rtol=0.01)
 
 
+def test_entmc_vbmc_overlapping_mixture():
+    # Check gradients with multiple Gaussians that have overlapping supports
+    np.random.seed(42)
+    D, K, Ns = 3, 2, 1e5
+    vp = VariationalPosterior(D, K)
+    vp.mu = np.random.uniform(-1, 1, size=(D, K))
+    vp.sigma = np.ones(K) + 0.2 * np.random.rand(K)
+    vp.lambd = np.ones(D) + 0.2 * np.random.rand(D)
+    vp.eta = np.random.rand(K)
+    vp.w = np.exp(vp.eta) / np.exp(vp.eta).sum()
+
+    theta0 = np.concatenate(
+        [x.flatten() for x in [vp.mu.transpose(), vp.sigma, vp.lambd, vp.w]]
+    )
+
+    f = lambda theta: entmc_vbmc_wrapper(theta, D, K, Ns, "H")
+    f_grad = lambda theta: entmc_vbmc_wrapper(theta, D, K, Ns, "dH")
+
+    assert check_grad(f, f_grad, theta0, rtol=0.01, atol=0.01)
+
+
 def test_entmc_vbmc_matlab():
-    # If exact is True, random seeds and samples should be the same 
-    # with MATLAB version, i.e. entmc_vbmc.py need to be modified a bit
-    # (see comments in entmc_vbmc.py)
+    # If exact is True, random seeds and samples should be the same
+    # with MATLAB version, i.e. entmc_vbmc.py need to be modified a
+    # bit: epsilon[: Ns // 2, :] = randn2(D, Ns // 2).transpose()
     exact = False
     mat = loadmat("./pyvbmc/entropy/entropy-test.mat")
     D = mat["D"].item()
@@ -147,3 +171,7 @@ def test_entmc_vbmc_grad_flags():
     grad_flags = tuple([False] * 3) + (True,)
     _, dH = entmc_vbmc(vp, Ns=1e5, grad_flags=grad_flags)
     assert dH.shape == (K,)
+
+
+if __name__ == "__main__":
+    test_entmc_vbmc_overlapping_mixture()