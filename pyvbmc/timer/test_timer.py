ea8fa22eb18efe8eade1efd5c27a8d9182826d5f
pipme <sjtulck@gmail.com>
2021-06-24 14:27:16 +0800

    fix: quick fix for test_timer.py

--- pyvbmc/timer/test_timer.py
+++ pyvbmc/timer/test_timer.py
@@ -5,7 +5,7 @@ def test_start_stop_get_normal():
     timer = Timer()
     timer.start_timer("test")
     timer.stop_timer("test")
-    assert timer.get_duration("test") > 0
+    assert timer.get_duration("test") >= 0
 
 
 def test_start_stop_get_reset():
@@ -16,8 +16,8 @@ def test_start_stop_get_reset():
     timer.start_timer("test")
     timer.stop_timer("test")
     timer2 = timer.get_duration("test")
-    assert timer2 > 0
-    assert timer2 < timer1 + timer2
+    assert timer2 >= 0
+    assert timer2 <= timer1 + timer2
 
 
 def test_timer_not_existing():
@@ -34,5 +34,5 @@ def test_timer_two_timers():
     timer.stop_timer("testimer1")
     timer1 = timer.get_duration("testimer1")
     timer2 = timer.get_duration("testimer2")
-    assert timer1 > 0 and timer2 > 0
-    assert timer1 > timer2
+    assert timer1 >= 0 and timer2 >= 0
+    assert timer1 >= timer2
