d3d665bd61c1696e8585cf8aeb6cd4975f468e2c
Aarnos Mikko <maarnos@lx1-500-24383.atm.helsinki.fi>
2021-07-23 16:29:49 +0300

    feat: added _vp_bound_loss feat: added _soft_bound_loss

--- pyvbmc/vbmc/variational_optimization.py
+++ pyvbmc/vbmc/variational_optimization.py
@@ -73,3 +73,112 @@ def optimize_vp(vp, gp, Nfastopts, Nslowopts, K=None):
     varss = []
     pruned = 0
     return varss, pruned
+    
+def _vp_bound_loss(
+    vp, theta, theta_bnd, tol_con=1e-3, compute_grad=True
+):
+    """
+    Variational paramtere loss function for soft optimization bounds.
+    """
+
+    if vp.optimize_mu:
+        mu = theta[: vp.D * vp.K]
+        start_idx = vp.D * vp.K
+    else:
+        mu = vp.mu.flatten()
+        start_idx = 0
+
+    if vp.optimize_sigma:
+        ln_sigma = theta[start_idx : start_idx + vp.K]
+        start_idx += vp.K
+    else:
+        ln_sigma = np.log(vp.sigma.flatten())
+
+    if vp.optimize_lambd:
+        ln_lambd = theta[start_idx : start_idx + vp.D].T
+    else:
+        ln_lambd = np.log(vp.lambd.flatten())
+
+    if vp.optimize_weights:
+        eta = theta[-vp.K :]
+    else:
+        eta = None
+
+    ln_scale = np.reshape(ln_lambd, (-1, 1)) + np.reshape(
+        ln_sigma, (1, -1)
+    )
+    theta_ext = []
+    if vp.optimize_mu:
+        theta_ext.append(mu.flatten())
+    if vp.optimize_sigma or vp.optimize_lambda:
+        theta_ext.append(ln_scale.flatten())
+    if vp.optimize_weights:
+        theta_ext.append(eta.flatten())
+    theta_ext = np.concatenate(theta_ext)
+
+    if compute_grad:
+        L, dL = _soft_bound_loss(
+            theta_ext,
+            theta_bnd["lb"].flatten(),
+            theta_bnd["ub"].flatten(),
+            tol_con,
+            compute_grad=True,
+        )
+
+        dL_new = np.array([])
+        if vp.optimize_mu:
+            dL_new = np.concatenate(
+                (dL_new, dL[0 : vp.D * vp.K].flatten())
+            )
+            start_idx = vp.D * vp.K
+        else:
+            start_idx = 0
+
+        if vp.optimize_sigma or vp.optimize_lambda:
+            dlnscale = np.reshape(
+                dL[start_idx : start_idx + vp.D * vp.K], (vp.D, vp.K)
+            )
+
+            if vp.optimize_sigma:
+                dL_new = np.concatenate((dL_new, np.sum(dlnscale, axis=0)))
+
+            if vp.optimize_lambda:
+                dL_new = np.concatenate((dL_new, np.sum(dlnscale, axis=1)))
+
+        if vp.optimize_weights:
+            dL_new = np.concatenate((dL_new, dL[-vp.K :].flatten()))
+
+        return L, dL_new
+
+    L = _soft_bound_loss(
+        theta_ext,
+        theta_bnd["lb"].flatten(),
+        theta_bnd["ub"].flatten(),
+        tol_con,
+    )
+    
+    return L
+
+def _soft_bound_loss(x, slb, sub, tol_con=1e-3, compute_grad=False):
+    """
+    Loss function for soft bounds for function minimization.
+    """
+    ell = (sub - slb) * tol_con
+    y = 0
+    dy = np.zeros(x.shape)
+
+    idx = x < slb
+    if np.any(idx):
+        y += 0.5 * np.sum((slb[idx] - x[idx]) / ell[idx] ** 2)
+        if compute_grad:
+            dy[idx] = (x[idx] - slb[idx]) / ell[idx] ** 2
+
+    idx = x > sub
+    if np.any(idx):
+        y += 0.5 * np.sum((x[idx] - sub[idx]) / ell[idx] ** 2)
+        if compute_grad:
+            dy[idx] = (x[idx] - sub[idx]) / ell[idx] ** 2
+
+    if compute_grad:
+        return y, dy
+    return y
