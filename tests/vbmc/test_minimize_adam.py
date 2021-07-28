f9773b360e6cf8e6ae11c24550ff892766491f49
Aarnos Mikko <maarnos@lx1-500-24383.atm.helsinki.fi>
2021-07-28 10:10:44 +0300

    feat: added two tests that have noise for ADAM optimizer

--- tests/vbmc/test_minimize_adam.py
+++ tests/vbmc/test_minimize_adam.py
@@ -14,6 +14,21 @@ def test_minimize_adam_sphere():
     assert np.all(np.abs(x) < 0.1)
     assert np.abs(y) < 0.001
 
+    
+def test_minimize_adam_sphere_with_noise():
+    # ADAM is supposed to work for noisy (unbiased) estimates
+    # of the gradient, so it is OK but not particularly good for
+    # deterministic optimization as in the other tests.
+    # Here we have i.i.d. Gaussian noise at each gadient with
+    # variance not too small.
+    f = lambda x_: (np.sum(x_ ** 2), 2 * x_ + np.random.normal(scale=3, size=x_.shape))
+    x0 = np.array([-3.0, -4.0])
+
+    x, y, _, _, _ = minimize_adam(f, x0)
+    
+    assert np.all(np.abs(x) < 0.5)
+    assert np.abs(y) < 0.1
+
 
 def test_minimize_adam_matyas():
     def f(x_):
@@ -31,7 +46,23 @@ def test_minimize_adam_matyas():
 
     assert np.all(np.abs(x) < 0.1)
     assert np.abs(y) < 0.001
+    
+def test_minimize_adam_matyas_with_noise():
+    def f(x_):
+        val = 0.26 * (x_[0] ** 2 + x_[1] ** 2) - 0.48 * x_[0] * x_[1]
+        grad_1 = 0.52 * x_[0] - 0.48 * x_[1]
+        grad_2 = 0.52 * x_[1] - 0.48 * x_[0]
+        grad = np.array([grad_1, grad_2]) + np.random.normal(scale=3, size=(2,))
+        return val, grad
 
+    x0 = np.array([-0.3, -0.4])
+    lb = np.array([-10.0, -10.0])
+    ub = np.array([10.0, 10.0])
+
+    x, y, _, _, _ = minimize_adam(f, x0, lb, ub)
+
+    assert np.all(np.abs(x) < 0.5)
+    assert np.abs(y) < 0.1
 
 def test_minimize_adam_rosen():
     f = lambda x_: (sp.optimize.rosen(x_), sp.optimize.rosen_der(x_))
