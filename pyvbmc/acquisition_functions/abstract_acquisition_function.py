47fea9f0678620a7490ce2bdce6c87abcbbfb985
Marlon Tobaben <24623119+Solosneros@users.noreply.github.com>
2021-08-09 16:51:39 +0200

    feat: add basic version of __call__ to abstract_acquisition_function

--- pyvbmc/acquisition_functions/abstract_acquisition_function.py
+++ pyvbmc/acquisition_functions/abstract_acquisition_function.py
@@ -1,4 +1,9 @@
-from abc import ABC
+import sys
+from abc import ABC, abstractmethod
+
+import gpyreg as gpr
+import numpy as np
+from pyvbmc.variational_posterior import VariationalPosterior
 
 
 class AbstractAcquisitionFunction(ABC):
@@ -15,3 +20,127 @@ class AbstractAcquisitionFunction(ABC):
             A dict containing information about the acquisition function.
         """
         return self.acq_info
+
+    def __call__(
+        self,
+        Xs: np.ndarray,
+        gp: gpr.GP,
+        vp: VariationalPosterior,
+        optim_state: dict,
+    ):
+        """
+        Calculate the acquisition function for the given inputs.
+
+        Parameters
+        ----------
+        Xs : np.ndarray
+            Input points.
+        gp : gpr.GP
+            The GaussianProcess of the VBMC instance this function is
+            called from.
+        vp : VariationalPosterior
+            The VariationalPosterior of the VBMC instance this function is
+            called from.
+        optim_state : dict
+            The optim_steate of the VBMC instance this function is
+            called from.
+
+        Returns
+        -------
+        acq : np.ndarray
+            The output of the acquisition function.
+        """
+
+        # Map integer inputs
+        Xs = _real2int(
+            Xs, vp.parameter_transformer, optim_state.get("integervars")
+        )
+
+        # Compute GP posterior predictive mean and variance
+
+        if (
+            hasattr(vp, "delta")
+            and vp.delta is not None
+            and np.any(vp.delta > 0)
+        ):
+            # Quadrature mean and variance for each hyperparameter sample
+            f_mu, f_s2 = gp.quad(
+                mu=Xs,
+                sigma=vp.delta.T,
+                compute_var=True,
+                separate_samples=True,
+            )
+        else:
+            # GP mean and variance for each hyperparameter sample
+            f_mu, f_s2 = gp.predict(x_star=Xs, separate_samples=True)
+
+        # Compute total variance
+        Ns = f_mu.shape[1]
+        f_bar = np.sum(f_mu, axis=1, keepdims=True) / Ns  # Mean across samples
+        var_bar = np.sum(f_s2, axis=1) / Ns  # Average variance across samples
+
+        # Sample variance
+        if Ns > 1:
+            var_f = np.sum((f_mu - f_bar) ** 2, axis=1) / (Ns - 1)
+        else:
+            var_f = 0
+
+        var_tot = var_f + var_bar  # Total variance
+
+        # Compute acquisition function
+        acq = self._compute_acquisition_function(
+            Xs, vp, gp, optim_state, f_mu, f_s2, f_bar, var_tot
+        )
+
+        # Regularization: penalize points where GP uncertainty
+        # is below threshold
+        if optim_state.get("variance_regularized_acq_fcn"):
+            # Try not to go below this variance
+            tol_var = optim_state.get("tol_gp_var")
+            idx_gp_uncertainty = var_tot < tol_var
+
+            if np.any(idx_gp_uncertainty):
+                if "log_flag" in self.acq_info and self.acq_info.get(
+                    "log_flag"
+                ):
+                    acq[idx_gp_uncertainty] += (
+                        tol_var / var_tot[idx_gp_uncertainty] - 1
+                    )
+                else:
+                    acq[idx_gp_uncertainty] *= np.exp(
+                        -(tol_var / var_tot[idx_gp_uncertainty] - 1)
+                    )
+
+        realmin = sys.float_info.min
+        acq = np.maximum(acq, realmin)
+
+        # Hard bound checking: discard points too close to bounds
+        X_orig = vp.parameter_transformer.inverse(Xs)
+        idx_bounds = np.logical_or(
+            np.any(X_orig < optim_state.get("lb_eps_orig"), axis=1),
+            np.any(X_orig > optim_state.get("ub_eps_orig"), axis=1),
+        )
+        acq[idx_bounds] = np.Inf
+
+        return acq
+
+    @abstractmethod
+    def _compute_acquisition_function(
+        self,
+        Xs: np.ndarray,
+        vp: VariationalPosterior,
+        gp: gpr.GP,
+        optimState: dict,
+        f_mu: np.ndarray,
+        f_s2: np.ndarray,
+        f_bar: np.ndarray,
+        var_tot: float,
+    ):
+        """
+        Abstract method that must be implemented in each subclass. It computes
+        the value of the acquisition function.
+        """
+
+
+def _real2int(X, parameter_transformer, integervars):
+    return X
--- tests/acquisition_functions/test_abstract_acquisition_function.py
+++ tests/acquisition_functions/test_abstract_acquisition_function.py
@@ -1,13 +1,170 @@
+import gpyreg as gpr
+import numpy as np
 from pyvbmc.acquisition_functions import AbstractAcquisitionFunction
+from pyvbmc.variational_posterior import VariationalPosterior
 
 
 def test_acq_info():
     class BasicAcqClass(AbstractAcquisitionFunction):
         def _compute_acquisition_function(
-            self, Xs, vp, gp, optimState, fmu, fs2, fbar, vtot
+            self, Xs, vp, gp, optimState, f_mu, f_s2, f_bar, var_tot
         ):
             pass
 
     acq_fcn = BasicAcqClass()
     assert isinstance(acq_fcn.acq_info, dict)
     assert isinstance(acq_fcn.get_info(), dict)
+
+
+def create_gp(D=3):
+    gp = gpr.GP(
+        D=D,
+        covariance=gpr.covariance_functions.SquaredExponential(),
+        mean=gpr.mean_functions.NegativeQuadratic(),
+        noise=gpr.noise_functions.GaussianNoise(constant_add=True),
+    )
+    return gp
+
+
+def test__call__simple(mocker):
+    """
+    Test only the main branches of the function.
+    """
+
+    class BasicAcqClass(AbstractAcquisitionFunction):
+        def _compute_acquisition_function(
+            self, Xs, vp, gp, optimState, f_mu, f_s2, f_bar, var_tot
+        ):
+            return np.ones(Xs.shape[0])
+
+    M = 20
+    Xs = np.ones((M, 3))
+
+    mocker.patch(
+        "gpyreg.GP.predict",
+        return_value=(np.ones((M, 2)), np.zeros((M, 2))),
+    )
+
+    acq_fcn = BasicAcqClass()
+    optim_state = dict()
+    optim_state["integervars"] = None
+    optim_state["variance_regularized_acq_fcn"] = False
+    # no constraints for test
+    optim_state["lb_eps_orig"] = -np.inf
+    optim_state["ub_eps_orig"] = np.inf
+    vp = VariationalPosterior(3)
+    acq = acq_fcn(Xs, create_gp(3), vp, optim_state)
+
+    assert acq.shape == (M,)
+
+
+def test__call_constraints(mocker):
+    """
+    Test hard bound checking: discard points too close to bounds
+    """
+
+    class BasicAcqClass(AbstractAcquisitionFunction):
+        def _compute_acquisition_function(
+            self, Xs, vp, gp, optimState, f_mu, f_s2, f_bar, var_tot
+        ):
+            return np.ones(Xs.shape[0])
+
+    M = 20
+    Xs = np.ones((M, 3))
+
+    mocker.patch(
+        "gpyreg.GP.predict",
+        return_value=(np.ones((M, 2)), np.zeros((M, 2))),
+    )
+
+    acq_fcn = BasicAcqClass()
+    optim_state = dict()
+    optim_state["integervars"] = None
+    optim_state["variance_regularized_acq_fcn"] = False
+
+    # set constraints for this test
+    optim_state["lb_eps_orig"] = 1000
+    optim_state["ub_eps_orig"] = 1001
+    vp = VariationalPosterior(3)
+    acq = acq_fcn(Xs, create_gp(3), vp, optim_state)
+
+    assert acq.shape == (M,)
+    assert np.all(acq == np.inf)
+
+
+def test__call_quad(mocker):
+    """
+    Quadrature mean and variance for each hyperparameter sample by
+    assigning vp.delta = np.ones(1, 2)
+    """
+
+    class BasicAcqClass(AbstractAcquisitionFunction):
+        def _compute_acquisition_function(
+            self, Xs, vp, gp, optimState, f_mu, f_s2, f_bar, var_tot
+        ):
+            return np.ones(Xs.shape[0])
+
+    M = 1
+
+    Xs = np.ones((M, 3))
+
+    mocker.patch(
+        "gpyreg.GP.quad",
+        return_value=(np.ones((M, 1)), np.zeros((M, 1))),
+    )
+
+    acq_fcn = BasicAcqClass()
+    optim_state = dict()
+    optim_state["integervars"] = None
+    optim_state["variance_regularized_acq_fcn"] = False
+    # no constraints for test
+    optim_state["lb_eps_orig"] = -np.inf
+    optim_state["ub_eps_orig"] = np.inf
+    vp = VariationalPosterior(3)
+    # assign delta for test
+    vp.delta = np.ones((1, 2))
+    acq = acq_fcn(Xs, create_gp(3), vp, optim_state)
+
+    assert acq.shape == (M,)
+
+
+def test__call__regularization(mocker):
+    """
+    Test regularization (penalize points where GP uncertainty is below
+    threshold).
+    """
+
+    class BasicAcqClass(AbstractAcquisitionFunction):
+        def _compute_acquisition_function(
+            self, Xs, vp, gp, optimState, f_mu, f_s2, f_bar, var_tot
+        ):
+            return np.ones(Xs.shape[0])
+
+    M = 20
+    Xs = np.ones((M, 3))
+
+    mocker.patch(
+        "gpyreg.GP.predict",
+        return_value=(np.ones((M, 2)) * 5, np.zeros((M, 2))),
+    )
+
+    acq_fcn = BasicAcqClass()
+    optim_state = dict()
+    optim_state["integervars"] = None
+    optim_state["variance_regularized_acq_fcn"] = True
+    optim_state["tol_gp_var"] = 2000
+
+    # no constraints for test
+    optim_state["lb_eps_orig"] = -np.inf
+    optim_state["ub_eps_orig"] = np.inf
+    vp = VariationalPosterior(3)
+
+    # no logflag
+    acq_fcn.acq_info["log_flag"] = False
+    acq = acq_fcn(Xs, create_gp(3), vp, optim_state)
+    assert acq.shape == (M,)
+
+    # logflag
+    acq_fcn.acq_info["log_flag"] = True
+    acq = acq_fcn(Xs, create_gp(3), vp, optim_state)
+    assert acq.shape == (M,)
