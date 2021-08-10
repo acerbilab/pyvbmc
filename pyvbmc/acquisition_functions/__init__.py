ee74f988f864310658a055fc0585b2ff8f172ead
Marlon Tobaben <24623119+Solosneros@users.noreply.github.com>
2021-08-10 17:13:07 +0200

    feat: add acq_fcn via vanilla uncertainty sampling

--- pyvbmc/acquisition_functions/__init__.py
+++ pyvbmc/acquisition_functions/__init__.py
@@ -1,3 +1,4 @@
 # __init__.py
 from .abstract_acq_fcn import AbstractAcqFcn
 from .acq_fcn import AcqFcn
+from .acq_fnc_vanilla import AcqFcnVanilla
---
+++ pyvbmc/acquisition_functions/acq_fnc_vanilla.py
@@ -0,0 +1,40 @@
+import sys
+
+import gpyreg as gpr
+import numpy as np
+from pyvbmc.function_logger import FunctionLogger
+from pyvbmc.variational_posterior import VariationalPosterior
+
+from .abstract_acq_fcn import AbstractAcqFcn
+
+
+class AcqFcnVanilla(AbstractAcqFcn):
+    """
+    Acquisition function via vanilla uncertainty sampling.
+    """
+
+    def _compute_acquisition_function(
+        self,
+        Xs: np.ndarray,
+        vp: VariationalPosterior,
+        gp: gpr.GP,
+        function_logger: FunctionLogger,
+        optim_state: dict,
+        f_mu: np.ndarray,
+        f_s2: np.ndarray,
+        f_bar: np.ndarray,
+        var_tot: np.ndarray,
+    ):
+        """
+        Compute the value of the acquisition function.
+        """
+        # Xs is in *transformed* coordinates
+
+        # Probability density of variational posterior at test points
+        realmin = sys.float_info.min
+        p = np.maximum(vp.pdf(Xs, origflag=False), realmin)
+
+        # Uncertainty search
+        acq = -var_tot * p ** 2
+
+        return acq
