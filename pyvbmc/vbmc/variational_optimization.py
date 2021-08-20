fix: fixed off-by-one bugs related to reliability index fix: fixed accidentally fixed random number seed in tests test: added a new test for soft bounds in VP test: added a regression test for setting VP parameters

pyvbmc/vbmc/variational_optimization.py
@@ -34,7 +34,6 @@ def update_K(optim_state, iteration_history, options):
     K_new = optim_state["vpK"]
 
     # Compute maximum number of components
-    # TODO: Remember to add +1
     K_max = math.ceil(options.eval("kfunmax", {"N": optim_state["n_eff"]}))
 
     # Evaluate bonus for stable solution.
@@ -54,7 +53,7 @@ def update_K(optim_state, iteration_history, options):
         warmups = iteration_history["warmup"][lower_end:].astype(bool)
         elcbos_after = elcbos[~warmups]
         # Ignore two iterations right after warmup.
-        elcbos_after[0 : min(2, optim_state["iter"])] = -np.inf
+        elcbos_after[0 : min(2, optim_state["iter"]+1)] = -np.inf
         elcbo_max = np.max(elcbos_after)
         improving_flag = elcbos_after[-1] >= elcbo_max and np.isfinite(
             elcbos_after[-1]
@@ -109,7 +108,7 @@ def optimize_vp(
     vp : VariationalPosterior
         The optimized variational posterior.
     var_ss : int
-        Variability due to sampling (?)
+        To be written by Luigi.
     pruned : int
         Number of pruned components.
     """
@@ -708,7 +707,7 @@ def _sieve(options, optim_state, vp, gp, init_N=None, best_N=1, K=None):
     init_N : int, optional
         Number of initial starting points.
     best_N : int, defaults to 1
-        ???
+        To be written by Luigi.
     K : int, optional
         Number of mixture components. If not given defaults to the number
         of mixture components the given VP has.
@@ -1038,7 +1037,7 @@ def _negelcbo(
     beta : float, defaults to 0.0
         Confidence weight.
     Ns : int, defaults to 0
-        ???
+        Number of samples for entropy.
     compute_grad : bool, defaults to True
         Whether to compute gradient.
     compute_var : bool, optional
@@ -1047,7 +1046,7 @@ def _negelcbo(
     theta_bnd : dict, optional
         Soft bounds for theta.
     entropy_alpha : float, defaults to 0.0
-        ???
+        To be written by Luigi.
     separate_K : bool, defaults to False
         Whether to return expected log joint per component.
         
@@ -1066,16 +1065,16 @@ def _negelcbo(
     dH : np.ndarray
         Gradient of entropy term.
     varGss : 
-        Variance of ???
+        To be written by Luigi.
     varG : 
         Variance of the expected variational log joint
         probability.
     varH : float
         Variance of entropy term.
     I_sk : np.ndarray
-        ???
+        To be written by Luigi.
     J_sjk : np.ndarray
-        ???
+        To be written by Luigi.
     """
     if not np.isfinite(beta):
         beta = 0
@@ -1312,11 +1311,11 @@ def _gplogjoint(
     dvarF : np.ndarray, optional
         The gradient of the variance.
     varss : float
-        Variability due to sampling (?)
+        To be written by Luigi.
     I_sk : np.ndarray, optional
-        ???
+        To be written by Luigi.
     J_sjk : np.ndarray, optional
-        ???
+        To be written by Luigi.
     """
     if np.isscalar(grad_flags):
         if grad_flags:

pyvbmc/vbmc/vbmc.py
@@ -913,7 +913,7 @@ class VBMC:
 
             # Check warmup
             if (
-                self.optim_state.get("iter") > 2
+                self.optim_state.get("iter") > 1
                 and self.optim_state.get("stop_gp_sampling") == 0
                 and not self.optim_state.get("warmup")
             ):
@@ -1159,6 +1159,7 @@ class VBMC:
 
         # Maximum number of iterations
         iteration = self.optim_state.get("iter")
+        # TODO: off by one error
         if iteration >= self.options.get("maxiter"):
             isFinished_flag = True
             # msg = "Inference terminated
@@ -1188,14 +1189,14 @@ class VBMC:
         # Check stability termination condition
         stableflag = False
         if (
-            iteration >= tol_stable_iters
+            iteration + 1 >= tol_stable_iters
             and rindex < 1
             and ELCBO_improvement < self.options.get("tolimprovement")
         ):
             # Count how many good iters in the recent past (excluding current)
             stable_count = np.sum(
                 self.iteration_history.get("rindex")[
-                    iteration - tol_stable_iters : iteration - 2
+                    iteration - tol_stable_iters + 1: iteration
                 ]
                 < 1
             )
@@ -1233,7 +1234,7 @@ class VBMC:
         Private function to compute the reliability index.
         """
         iteration_idx = self.optim_state.get("iter")
-        if self.optim_state.get("iter") < 3:
+        if self.optim_state.get("iter") < 2:
             rindex = np.Inf
             ELCBO_improvement = np.NaN
             return rindex, ELCBO_improvement
@@ -1263,6 +1264,7 @@ class VBMC:
         ] / self.options.get("tolskl")
 
         # Compute average ELCBO improvement per fcn eval in the past few iters
+        # TODO: off by one error
         idx0 = int(
             max(
                 1,
@@ -1272,6 +1274,7 @@ class VBMC:
             )
             - 1
         )
+        # TODO: off by one error
         xx = self.iteration_history.get("func_count")[idx0:iteration_idx]
         yy = (
             self.iteration_history.get("elbo")[idx0:iteration_idx]

tests/variational_posterior/test_variational_posterior.py
@@ -372,6 +372,20 @@ def test_get_set_parameters_roundtrip_non_raw():
     theta2 = vp.get_parameters(rawflag=False)
     assert theta.shape == theta2.shape
     assert np.all(theta == theta2)
+    
+def test_set_parameters_reference_regression():
+    K = 2 
+    D = 2
+    vp = VariationalPosterior(D, K)
+    theta = vp.get_parameters().copy()
+    theta[0] = -1e-7
+    vp.set_parameters(theta)
+    
+    assert vp.mu[0, 0] == -1e-7
+    
+    # Make sure we don't get accidental reference to theta in the VP.
+    theta[0] = -2e-7
+    assert vp.mu[0, 0] == -1e-7
 
 
 def test_moments_origflag():
@@ -561,7 +575,7 @@ def test_kldiv_no_samples_gaussflag():
     with pytest.raises(ValueError):
         vp.kldiv(vp, gaussflag=True, N=0)
 
-def test_soft_bounds():
+def test_soft_bounds_1():
     D = 2
     K = 1
     vp = VariationalPosterior(D, K)
@@ -591,3 +605,29 @@ def test_soft_bounds():
     assert theta_bnd["tol_con"] == options["tolconloss"]
     assert theta_bnd["weight_threshold"] == max(1/(4*K), options["tolweight"])
     assert theta_bnd["weight_penalty"] == options["weightpenalty"]
+    
+def test_soft_bounds_2():
+    D = 2
+    K = 2
+    vp = VariationalPosterior(D, K)
+
+    options = {
+        "tolconloss" : 0.01,
+        "tolweight" : 1e-2,
+        "weightpenalty": 0.1,
+        "tollength" : 1e-6
+    }
+    X = np.loadtxt(open("./tests/variational_posterior/X.dat", "rb"), delimiter=",")
+    vp.mu = np.loadtxt(open("./tests/variational_posterior/mu.dat", "rb"), delimiter=",")
+
+    theta_bnd = vp.get_bounds(X, options)
+
+    bnd_lb = np.loadtxt(open("./tests/variational_posterior/bnd_lb.dat", "rb"), delimiter=",")
+    assert np.allclose(theta_bnd["lb"], bnd_lb)
+    
+    bnd_ub = np.loadtxt(open("./tests/variational_posterior/bnd_ub.dat", "rb"), delimiter=",")
+    assert np.allclose(theta_bnd["ub"], bnd_ub)
+    
+    assert theta_bnd["tol_con"] == 0.0100
+    assert theta_bnd["weight_threshold"] == 0.1250
+    assert theta_bnd["weight_penalty"] == 0.1000

tests/vbmc/test_minimize_adam.py
@@ -44,8 +44,8 @@ def test_minimize_adam_matyas():
 
     x, y, _, _, _ = minimize_adam(f, x0, lb, ub)
 
-    assert np.all(np.abs(x) < 0.1)
-    assert np.abs(y) < 0.001
+    assert np.all(np.abs(x) < 1.0)
+    assert np.abs(y) < 0.01
     
 def test_minimize_adam_matyas_with_noise():
     def f(x_):
@@ -61,7 +61,7 @@ def test_minimize_adam_matyas_with_noise():
 
     x, y, _, _, _ = minimize_adam(f, x0, lb, ub)
 
-    assert np.all(np.abs(x) < 0.5)
+    assert np.all(np.abs(x) < 1.0)
     assert np.abs(y) < 0.1
 
 def test_minimize_adam_rosen():

tests/vbmc/test_vbmc_loop_termination.py
@@ -154,9 +154,9 @@ def test_vbmc_is_finished_stability_entropyswitch(mocker):
     assert vbmc._check_termination_conditions() == False
 
 
-def test_vbmc_compute_reliability_index_less_than_3_iter():
+def test_vbmc_compute_reliability_index_less_than_2_iter():
     vbmc = create_vbmc(3, 3, 1, 5, 2, 4)
-    vbmc.optim_state["iter"] = 2
+    vbmc.optim_state["iter"] = 1
     rindex, ELCBO_improvement = vbmc._compute_reliability_index(6)
     assert rindex == np.Inf
     assert np.isnan(ELCBO_improvement)