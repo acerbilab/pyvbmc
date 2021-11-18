2769e8211be2b851631ffa326ee7c289ad6181fa
Marlon Tobaben <24623119+Solosneros@users.noreply.github.com>
2021-11-18 13:32:42 +0200

    fix: remove duplicate code

--- docsrc/source/conf.py
+++ docsrc/source/conf.py
@@ -15,7 +15,6 @@ import sys
 import inspect
 
 sys.path.insert(0, os.path.abspath("../.."))
-sys.path.insert(0, os.path.abspath("../.."))
 
 # -- Project information -----------------------------------------------------
 
