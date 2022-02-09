fix: Fix nan appearing due to numerical issues with new ParameterTransformer.

pyvbmc/variational_posterior/variational_posterior.py
@@ -789,7 +789,7 @@ class VariationalPosterior:
         ## )
         # self.parameter_transformer.mu = np.zeros(self.D)
         # self.parameter_transformer.delta = np.ones(self.D)
-        yy = self.parameter_transformer(xx)
+        yy = self.parameter_transformer(xx, no_infs=True)
         [plb, pub] = np.quantile(yy, [0.05, 0.95], axis=0)
         delta_temp = pub-plb
         plb = plb - delta_temp/9
@@ -828,7 +828,7 @@ class VariationalPosterior:
         # y_orig = vbmc.optim_state["cache"]["y_orig"][X_flag]
         X_orig = vbmc.function_logger.X_orig[X_flag, :]
         y_orig = vbmc.function_logger.y_orig[X_flag].T
-        X = self.parameter_transformer(X_orig)
+        X = self.parameter_transformer(X_orig, no_infs=True)
         dy = self.parameter_transformer.log_abs_det_jacobian(X)
         y = y_orig + dy/T
         # print(y.shape)
@@ -843,7 +843,7 @@ class VariationalPosterior:
             # Copy probably unneccesary:
             x = np.copy(x)
             return self.parameter_transformer(
-                                    vp_old.parameter_transformer.inverse(x)
+                                    vp_old.parameter_transformer.inverse(x), no_infs=True
                                               )
         Nrnd = 1000
         xx = np.random.rand(Nrnd, self.D) * \
@@ -892,8 +892,9 @@ class VariationalPosterior:
         Ns_gp = len(vp_old.gp.posteriors)
         hyp_warped = np.zeros([Ncov + Nnoise + Nmean, Ns_gp])
 
+        hyps = vp_old.gp.get_hyperparameters(as_array = True)
         for s in range(Ns_gp):
-            hyp = vp_old.gp.posteriors[s].hyp
+            hyp = hyps[s]
             hyp_warped[:, s] = hyp.copy()
 
             # UpdateGP input length scales (Not needed for linear warping?)
@@ -935,7 +936,7 @@ class VariationalPosterior:
         # Update GP:
         # TODO: Check for correctness
         # self.gp.hyp = hyp_warped
-        vbmc.optim_state["hyp_dict"]["hyp"] = hyp_warped
+        vbmc.optim_state["hyp_dict"]["hyp"] = hyp_warped.T
         mu = vp_old.mu.T
         sigmalambda = (vp_old.lambd * vp_old.sigma).T
 

pyvbmc/vbmc/gaussian_process_train.py
@@ -118,7 +118,7 @@ def train_gp(
 
     # Build starting points
     # hyp0 = np.empty((0, np.size(hyp_dict["hyp"])))
-    hyp0 = np.empty((0, hyp_dict["hyp"].shape[0]))
+    hyp0 = np.empty((0, hyp_dict["hyp"].T.shape[0]))
     if gp_train["init_N"] > 0 and optim_state["iter"] > 0:
         # Be very careful with off-by-one errors compared to MATLAB in the
         # range here.
@@ -144,7 +144,7 @@ def train_gp(
             ]
     # Clunky workaround for different shaped arrays:
     if len(hyp_dict["hyp"].shape) == 2:
-        hyp0 = np.concatenate((hyp0, np.array(hyp_dict["hyp"]).T))
+        hyp0 = np.concatenate((hyp0, np.array(hyp_dict["hyp"])))
     else:
         hyp0 = np.concatenate((hyp0, np.array([hyp_dict["hyp"]])))
     hyp0 = np.unique(hyp0, axis=0)
@@ -639,6 +639,7 @@ def _get_gp_training_options(
 
     gp_train["n_samples"] = round(gp_s_N)
     gp_train["burn"] = round(gp_train["burn"])
+
 
     return gp_train
 