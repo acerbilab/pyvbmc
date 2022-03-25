f91d0956097d9191625c244b79bf1a6d9d1a1fcc
aloctavodia <aloctavodia@gmail.com>
2022-03-25 15:20:46 +0200

    pymc integration

--- pyvbmc/vbmc/__init__.py
+++ pyvbmc/vbmc/__init__.py
@@ -5,4 +5,5 @@ from .gaussian_process_train import train_gp
 from .variational_optimization import optimize_vp, update_K
 from .iteration_history import IterationHistory
 from .options import Options
+from .pymc_fit import pymc_fit
 from .vbmc import VBMC
