3a5a0f263d725233bce8604d2dde11db2ee3b8f8
Bobby Huggins <bobby.p.huggins@gmail.com>
2022-04-06 15:54:15 +0300

    Test: Add more complex test for AcqFsnNoisy.

--- tests/acquisition_functions/test_acq_fcn_noisy.py
+++ tests/acquisition_functions/test_acq_fcn_noisy.py
@@ -55,3 +55,57 @@ def test__call__(mocker):
 
     assert acq.shape == (M,)
     assert np.all(acq == -0.5)
+
+
+def test_complex__call__(mocker):
+    acqf = AcqFcnNoisy()
+    M = 4
+    Xs = np.ones((M, 3))
+
+    mocker.patch(
+        "gpyreg.GP.predict",
+        return_value=(np.arange(0, 2*M).reshape((M, 2), order="F")/13,
+                      np.arange(0, 2*M).reshape((M, 2), order="F")/23 ),
+    )
+
+    mocker.patch(
+        "pyvbmc.variational_posterior.VariationalPosterior.pdf",
+        return_value=np.arange(0, M).reshape((M, 1), order="F") * np.exp(-1)
+    )
+
+    optim_state = dict()
+    optim_state["integervars"] = None
+    optim_state["variance_regularized_acq_fcn"] = False
+    optim_state["gp_length_scale"] = np.exp(np.mean(np.arange(0, 3*2).reshape((3, 2), order="F"), axis=1)).T
+
+    # no constraints for test
+    optim_state["lb_eps_orig"] = -np.inf
+    optim_state["ub_eps_orig"] = np.inf
+
+    vp = VariationalPosterior(3)
+    function_logger = FunctionLogger(np.sum, 3, False, 0)
+    function_logger(np.ones(3))
+    function_logger(-np.ones(3))
+
+    gp = gpr.GP(
+        D=3,
+        covariance=gpr.covariance_functions.SquaredExponential(),
+        mean=gpr.mean_functions.NegativeQuadratic(),
+        noise=gpr.noise_functions.GaussianNoise(constant_add=True),
+    )
+
+    gp.temporary_data["sn2_new"] = np.exp(-1) *\
+        np.arange(1, M*2+1).reshape((M, 2), order="F")
+    gp.temporary_data["X_rescaled"] = Xs / optim_state["gp_length_scale"]
+
+    acq = acqf(Xs, gp, vp, function_logger, optim_state)
+
+    assert acq.shape == (M,)
+    print(acq)
+    acq_MATLAB = np.array([
+        -4.64016986306700e-311,
+        -0.00133615097976628,
+        -0.00414035897633624,
+        -0.00894224652574921
+    ])
+    assert np.allclose(acq, acq_MATLAB)
