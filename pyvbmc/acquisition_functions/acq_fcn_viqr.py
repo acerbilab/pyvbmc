c0dc7f4b0e2360746d9790de74e0c11d52c22b9d
Bobby Huggins <bobby.p.huggins@gmail.com>
2022-04-18 19:38:22 +0300

    fix: Add acq_fcn_viqr.py after merge mistake.

---
+++ pyvbmc/acquisition_functions/acq_fcn_viqr.py
@@ -0,0 +1,50 @@
+import sys
+
+import gpyreg as gpr
+import numpy as np
+from pyvbmc.function_logger import FunctionLogger
+from pyvbmc.variational_posterior import VariationalPosterior
+
+from .abstract_acq_fcn import AbstractAcqFcn
+
+
+class AcqFcnVIQR(AbstractAcqFcn):
+    """
+    Acquisition Function based on Variational Inter-Quantile Range
+    """
+
+    def __init__(self):
+        self.importance_sampling = True
+        self.importance_sampling_vp = False
+        self.variational_importance_sampling = True
+        self.log_flag = True
+
+    def _compute_acquisition_function(
+        self,
+        Xs: np.ndarray,
+        vp: VariationalPosterior,
+        gp: gpr.GP,
+        function_logger: FunctionLogger,
+        optim_state: dict,
+        f_mu: np.ndarray,
+        f_s2: np.ndarray,
+        f_bar: np.ndarray,
+        var_tot: np.ndarray,
+    ):
+        """
+        Compute the value of the acquisition function.
+        """
+        # Xs is in *transformed* coordinates
+
+        # Probability density of variational posterior at test points
+        realmin = sys.float_info.min
+        p = np.ravel(np.maximum(vp.pdf(Xs, origflag=False), realmin))
+
+        # Estimate observation noise at test points from nearest neighbor.
+        sn2 = super()._estimate_observation_noise(Xs, gp, optim_state)
+
+        z = function_logger.ymax
+
+        # Prospective uncertainty search corrected for noisy observations
+        acq = -var_tot * (1 - sn2 / (var_tot + sn2)) * np.exp(f_bar - z) * p
+        return acq
