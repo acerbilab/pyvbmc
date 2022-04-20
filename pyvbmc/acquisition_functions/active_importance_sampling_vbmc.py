eb05c540e92621d725be6eeda5676700798a801d
Bobby Huggins <bobby.p.huggins@gmail.com>
2022-04-20 13:01:53 +0300

    fix: Typos in importance sampling branches.

--- pyvbmc/acquisition_functions/active_importance_sampling_vbmc.py
+++ pyvbmc/acquisition_functions/active_importance_sampling_vbmc.py
@@ -45,10 +45,12 @@ def active_importance_sampling_vbmc(vp, gp, acqfcn, options):
 
     # Input space bounds and typical scales (for MCMC only)
     widths = np.std(gp.X, axis=0, ddof=1)
-    max_bnd = 0.5
+    max_bnd = 500
     diam = np.amax(gp.X, axis=0) - np.amin(gp.X, axis=0)
     LB = np.amin(gp.X, axis=0) - max_bnd * diam
     UB = np.amax(gp.X, axis=0) + max_bnd * diam
+    print(LB)
+    print(UB)
 
     active_is = {}
     active_is["log_weight"] = None
@@ -87,7 +89,7 @@ def active_importance_sampling_vbmc(vp, gp, acqfcn, options):
                 # Contrary to MATLAB, we are using simple slice sampling.
                 # Better (e.g. ensemble slice) sampling methods could
                 # later be implemented.
-                sampler = gpr.SliceSampler(log_p_fun, Xa, widths, LB, UB, sampler_opts)
+                sampler = gpr.slice_sample.SliceSampler(log_p_fun, Xa, widths, LB, UB, sampler_opts)
                 results = sampler.sample(Nmcmc_samples, thin, burn_in)
                 Xa = results["samples"]
                 # Xa = eis_sample_lite(log_p_fun, Xa, Nmcmc_samples, W, widths, LB, UB, sample_opts)
@@ -121,28 +123,47 @@ def active_importance_sampling_vbmc(vp, gp, acqfcn, options):
             vp_is = copy.deepcopy(vp)
             for i in range(len(scale_vec)):
                 vp_is.K = vp_is.K + vp.K
-                vp_is.w = np.append(vp_is.w, vp.w)
-                vp_is.mu = np.append(vp_is.mu, vp.mu)
-                vp_is.sigma = np.append(vp_is.sigma, vp.sigma)
+                vp_is.w = np.hstack((vp_is.w, vp.w))
+                vp_is.mu = np.hstack((vp_is.mu, vp.mu))
+                vp_is.sigma = np.hstack((vp_is.sigma,
+                                        vp.sigma**2 + scale_vec[i]**2))
             vp_is.w = vp_is.w / np.sum(vp_is.w)
 
             # Sample from smoothed posterior
-            Xa_vp = vp_is.sample(Nvp_samples, origflag=False)
+            Xa_vp, __ = vp_is.sample(Nvp_samples, origflag=False)
             ln_w, f_s2a_vp = activesample_proposalpdf(Xa_vp, gp, vp_is, w_vp, rect_delta, acqfcn, vp, isample_vp_flag)
-            active_is["ln_w"] = np.append(active_is["ln_w"], ln_w.T)
-            active_is["Xa"] = np.append(active_is["Xa"], Xa_vp)
-            active_is["f_s2a"] = np.append(active_is["f_s2a"], f_s2a_vp)
+            if active_is.get("ln_w") is None:
+                active_is["ln_w"] = ln_w.T
+            else:
+                active_is["ln_w"] = np.append(active_is["ln_w"], ln_w.T, axis=1)
+            if active_is.get("Xa") is None:
+                active_is["Xa"] = Xa_vp
+            else:
+                active_is["Xa"] = np.append(active_is["Xa"], Xa_vp, axis=0)
+            if active_is.get("f_s2a") is None:
+                active_is["f_s2a"] = f_s2a_vp
+            else:
+                active_is["f_s2a"] = np.append(active_is["f_s2a"], f_s2a_vp, axis=0)
         else:
             vp_is = None
 
         # Box-uniform sampling around training inputes
         if Nbox_samples > 0:
-            jj = np.random.randint(0, len(gp.X), size=(1, Nbox_samples))
+            jj = np.random.randint(0, len(gp.X), size=(Nbox_samples,))
             Xa_box = gp.X[jj, :] + (2 * np.random.rand(jj.size, D) - 1) * rect_delta
-            ln_w, f_s2a_box = activesample_proposalpdf(Xa_box, vp_is, w_vp, rect_delta, acqfcn, vp, isample_vp_flag)
-            active_is["ln_w"] = np.append(active_is["ln_w"], ln_w.T)
-            active_is["Xa"] = np.append(active_is["Xa"], Xa_box)
-            active_is["f_s2a"] = np.append(active_is["f_s2a"], f_s2a_box)
+            ln_w, f_s2a_box = activesample_proposalpdf(Xa_box, gp, vp_is, w_vp, rect_delta, acqfcn, vp, isample_vp_flag)
+            if active_is.get("ln_w") is None:
+                active_is["ln_w"] = ln_w.T
+            else:
+                active_is["ln_w"] = np.append(active_is["ln_w"], ln_w.T, axis=1)
+            if active_is.get("Xa") is None:
+                active_is["Xa"] = Xa_box
+            else:
+                active_is["Xa"] = np.append(active_is["Xa"], Xa_box, axis=0)
+            if active_is.get("f_s2a") is None:
+                active_is["f_s2a"] = f_s2a_box
+            else:
+                active_is["f_s2a"] = np.append(active_is["f_s2a"], f_s2a_box, axis=0)
 
         active_is["ln_w"][~np.isfinite(active_is["ln_w"])] = -np.inf
 
@@ -160,7 +181,7 @@ def active_importance_sampling_vbmc(vp, gp, acqfcn, options):
             # Consider only one GP sample at a time
             gp1 = copy.deepcopy(gp)
             for s in range(Ns_gp):
-                gp1.post = gp.post[s]  # Assign current GP sample
+                gp1.posteriors = np.array([gp.posteriors[s]])  # Assign current GP sample
 
                 # quasi-random grid for D <= 2, not implemented
                 # See activeimportancesampling_vbmc.m, lines 159 to 165.
@@ -176,25 +197,31 @@ def active_importance_sampling_vbmc(vp, gp, acqfcn, options):
                 sampler_opts, __, __ = get_mcmc_opts(Nmcmc_samples)
 
                 # Not used (see comment below regarding simple slice sampling.)
-                Walkers = 2 * (D + 1)
+                # Walkers = 2 * (D + 1)
+                Walkers = 1
 
                 # Use importance sampling-resampling
-                f_mu, f_s2 = gp.predict(active_is_old["Xa"], separate_samples=True)
-                ln_w = active_is_old["ln_w"] + acqfcn.is_log_f2(f_mu, f_s2)
-                w = np.exp(ln_w - np.amax(ln_w, axis=1))
-                x0 = np.zeros(W, D)
+                f_mu, f_s2 = gp1.predict(active_is_old["Xa"], separate_samples=True)
+                ln_w = active_is_old["ln_w"][s, :].reshape(-1, 1)\
+                       + acqfcn.is_log_f2(f_mu, f_s2)
+                ln_w_max = np.amax(ln_w, axis=1).reshape(-1, 1)
+                assert np.all(ln_w_max != -np.inf)
+                w = np.exp(ln_w - ln_w_max).reshape(-1)
+                w = w / np.sum(w)
+                # x0 = np.zeros((Walkers, D))
                 # Select without replacement by weight w:
                 indices = np.random.choice(a=len(w), p=w, replace=False)
-                x0[indices, :] = active_is_old["Xa"][indices, :]
+                x0 = active_is_old["Xa"][indices, :]
+                print(x0)
 
                 # Contrary to MATLAB, we are using simple slice sampling.
                 # Better (e.g. ensemble slice) sampling methods could
                 # later be implemented.
-                sampler = gpr.SliceSampler(log_p_fun, x0, widths, LB, UB, sampler_opts)
+                sampler = gpr.slice_sample.SliceSampler(log_p_fun, x0, widths, LB, UB, sampler_opts)
                 results = sampler.sample(Nmcmc_samples, thin, burn_in)
                 Xa, log_p = results["samples"], results["f_vals"]
                 # Xa, log_p = eis_sample_lite(log_p_fun, x0, Nmcmc_samples, W, widths, LB, UB, sample_opts)
-                f_mu, f_s2 = gp.predict(Xa, separate_samples=True)
+                f_mu, f_s2 = gp1.predict(Xa, separate_samples=True)
 
                 # Fixed log weight for importance sampling (log fixed integrand)
                 if isample_vp_flag:
@@ -204,7 +231,7 @@ def active_importance_sampling_vbmc(vp, gp, acqfcn, options):
                 else:
                     ln_y = acqfcn.is_log_f1(None, f_mu, f_s2)
 
-                active_is["f_s2a"][:, s] = f_s2
+                active_is["f_s2a"][:, s] = f_s2.reshape(-1)
                 active_is["ln_w"][s, :] = ln_y.T - log_p.T
                 active_is["Xa"][:, :, s] = Xa
 
@@ -215,10 +242,10 @@ def active_importance_sampling_vbmc(vp, gp, acqfcn, options):
         (active_is["Xa"].shape[0], gp.X.shape[0], Ns_gp)
         )
     for s in range(Ns_gp):
-        if len(active_is["Xa"].shape) == 2:
-            Xa = active_is["Xa"]
-        else:
+        if active_is["Xa"].ndim == 3:
             Xa = active_is["Xa"][:, :, s]
+        else:
+            Xa = active_is["Xa"]
         cov_N = gp.covariance.hyperparameter_count(gp.D)
         hyp = gp.posteriors[s].hyp[0:cov_N]  # just covariance hyperparameters
         if isinstance(gp.covariance,
@@ -245,7 +272,7 @@ def activesample_proposalpdf(Xa, gp, vp_is, w_vp, rect_delta, acqfcn, vp, isampl
     -------
 
     """
-    N, D = gp.Xa.shape
+    N, D = gp.X.shape
     Na = Xa.shape[0]
 
     f_mu, f_s2 = gp.predict(Xa, separate_samples=True)
@@ -253,11 +280,11 @@ def activesample_proposalpdf(Xa, gp, vp_is, w_vp, rect_delta, acqfcn, vp, isampl
     Ntot = 1 + N  # Total number of mixture elements
 
     if w_vp < 1:
-        temp_lpdf = np.zeros(Na, Ntot)
+        temp_lpdf = np.zeros((Na, Ntot))
 
     # Mixture of variational posteriors
     if w_vp > 0:
-        temp_lpdf[:, 0] = vp_is.pdf(Xa, origflag=False, logflag=True)
+        temp_lpdf[:, 0] = vp_is.pdf(Xa, origflag=False, logflag=True).T
     else:
         temp_lpdf[:, 0] = -np.inf
 
@@ -273,16 +300,18 @@ def activesample_proposalpdf(Xa, gp, vp_is, w_vp, rect_delta, acqfcn, vp, isampl
     if w_vp < 1:
         VV = np.product(2 * rect_delta)
 
-        for ii in range(N):
-            temp_lpdf[:, ii+2] = np.log(
+        for i in range(N):
+            temp_lpdf[:, i+1] = np.log(
                 (1 - w_vp) * np.all(
-                    np.abs(Xa - gp.X[ii, :]) < rect_delta, axis=1
+                    np.abs(Xa - gp.X[i, :]) < rect_delta, axis=1
                 ) / VV / N
             )
 
         m_max = np.amax(temp_lpdf, axis=1)
-        l_pdf = np.log(np.sum(np.exp(temp_lpdf - m_max), axis=1))
-        ln_w = ln_y - (l_pdf + m_max)
+        assert np.all(m_max != -np.inf)
+        l_pdf = np.log(np.sum(np.exp(temp_lpdf - m_max.reshape(-1, 1)),
+                              axis=1))
+        ln_w = ln_y - (l_pdf + m_max).reshape(-1, 1)
     else:
         ln_w = ln_y - temp_lpdf
 
@@ -311,7 +340,7 @@ def log_isbasefun(x, acq_fcn, gp, vp=None):
         The proposal log pdf evaluated at the input points.
     """
 
-    f_mu, f_s2 = gp.predict(x)
+    f_mu, f_s2 = gp.predict(x.reshape(1, -1))
 
     if vp is None:
         return acq_fcn.is_log_f(0, f_mu, f_s2)
