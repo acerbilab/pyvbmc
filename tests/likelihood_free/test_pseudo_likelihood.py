3d6a314b64421fd34cef2b74462e417021db54fc
Bobby Huggins <bobby.p.huggins@gmail.com>
2022-04-28 12:39:57 +0300

    test: Minor change to pseudo-likelihood test.

--- tests/likelihood_free/test_pseudo_likelihood.py
+++ tests/likelihood_free/test_pseudo_likelihood.py
@@ -8,13 +8,14 @@ from pyvbmc.vbmc import VBMC
 
 
 def test_pseudo_likelihood_simple():
-    ll = pseudo_likelihood(lambda t: t, lambda d: d, data=0.0)
+    ll, v_scale, h_scale = pseudo_likelihood(lambda t: t, lambda d: d, data=0.0, return_scale=True)
     x1 = np.linspace(0, 0.9)
     x2 = np.linspace(0.9, 10.0)
     y1 = np.array([ll(xi) for xi in x1])
     y2 = np.array([ll(xi) for xi in x2])
     assert np.all(y1 == y1[0])  # Constant up to a * epsilon
     assert np.all(y2[1:] < y2[:-1])  # Decreasing after a*epsilon
+    assert np.allclose(y1, np.log(v_scale))
 
 
 def test_pseudo_likelihood_complex():
@@ -53,7 +54,7 @@ def test_pseudo_likelihood_complex():
         data_lls[i] = ll_data(theta)
         no_data_lls[i] = ll_no_data(theta, d_obs)
     assert np.allclose(data_lls, no_data_lls, rtol=1e-4)
-    assert np.allclose(data_lls, -np.log(v_scale), rtol=1e-4)
+    assert np.allclose(data_lls, np.log(v_scale), rtol=1e-4)
 
     N = 10000
     d_obs = np.sqrt(2) * sps.multivariate_normal.rvs(cov=np.eye(D), size=N)
@@ -101,7 +102,7 @@ def test_vbmc_optimize_pseudo_ll():
     N = 500
 
     sim_data = sps.multivariate_normal.rvs(cov=np.eye(D), size=N)
-    d_obs = sps.multivariate_normal.rvs(cov=np.eye(D), size=N)
+    d_obs = 2 * sps.multivariate_normal.rvs(cov=np.eye(D), size=N)
 
     def fake_sim(theta):  # Deterministic simulation
         return np.linalg.norm(theta) * sim_data
@@ -110,7 +111,7 @@ def test_vbmc_optimize_pseudo_ll():
     def fake_summary(d_theta):
         return np.mean(np.var(d_theta, axis=0))
 
-    llfun = pseudo_likelihood(fake_sim, fake_summary, data=d_obs, epsilon=0.1)
+    llfun = pseudo_likelihood(fake_sim, fake_summary, data=d_obs, epsilon=1.0)
 
     def ltarget(t):  # Pseudo-likelihood + wide prior
         return llfun(t) + sps.multivariate_normal.logpdf(t, cov=8 * np.eye(D))
