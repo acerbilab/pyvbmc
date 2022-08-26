5529f4b401486bd48dd8a7fe05396febff2136e2
Bobby Huggins <bobby.p.huggins@gmail.com>
2022-08-26 18:54:42 +0300

    chore: Delete helper scripts and files.

--- find_files.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Finds .py and .ini files to edit, in the appropriate directories, and
-# writes them to files.
-# Usage: ./find_files.sh
-find ./pyvbmc/ \( -type d -name .git -prune \) -o -type f -and \( -name "*.py" -or -name "*.ini" \) > ./replace_files_pyvbmc.txt
-find ./tests/ \( -type d -name .git -prune \) -o -type f -and \( -name "*.py" -or -name "*.ini" \) > ./replace_files_tests.txt
