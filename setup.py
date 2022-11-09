09f4551dff5a59141836e31fb345d175b2d50489
Bobby Huggins <bobby.p.huggins@gmail.com>
2022-11-09 21:55:11 +0200

    chore: move requirements to `pyproject.toml`.

--- setup.py
+++ setup.py
@@ -1,28 +1,6 @@
-from setuptools import find_packages, setup
+#!/usr/bin/env python
 
-setup(
-    name="pyvbmc",
-    version="0.1.0",
-    description="Variational Bayesian Monte Carlo",
-    packages=find_packages(),
-    install_requires=[
-        "numpy",
-        "scipy",
-        "matplotlib",
-        "pytest",
-        "numpydoc",
-        "cma",
-        "corner",
-        "imageio",
-        "kaleido",
-        "myst_nb>=0.17.1",
-        "numpydoc>=1.2.1",
-        "sphinx>=4.5.0",
-        "sphinx-book-theme>=0.3.3",
-        "pylint",
-        "pytest",
-        "pytest-cov",
-        "pytest-mock",
-        "pytest-rerunfailures",
-    ],
-)
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
